4545b0ba03714a6db5c9769d90c2aa8c14178b0f
Bartłomiej Łotys <bartlomiejlotys@gmail.com>
2019-03-29 18:03:59 +0100

    Klucze obce

--- Tworzenie_tabel.sql
+++ Tworzenie_tabel.sql
@@ -1,3 +1,9 @@
+USE master
+
+DROP DATABASE Firma
+CREATE DATABASE Firma
+USE Firma
+
 CREATE TABLE Produkty
 	(model varchar PRIMARY KEY NOT NULL,
 	producent text NOT NULL, 
@@ -21,9 +27,17 @@ CREATE TABLE Laptop
 	ekran integer NULL,
 	cena money NOT NULL);
 
-
 CREATE TABLE Drukarki
 	(model varchar PRIMARY KEY NOT NULL,
 	kolor bit NOT NULL, 
 	typ text NOT NULL, 
 	cena money NOT NULL);
+
+ALTER TABLE Drukarki
+	ADD CONSTRAINT fk_model_drukarki FOREIGN KEY (model) REFERENCES Produkty(model);
+
+ALTER TABLE PC
+	ADD CONSTRAINT fk_model_pc FOREIGN KEY (model) REFERENCES Produkty(model);
+
+ALTER TABLE Laptop
+	ADD CONSTRAINT fk_model_laptop FOREIGN KEY (model) REFERENCES Produkty(model);
